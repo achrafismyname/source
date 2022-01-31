e07345d0459f06b0f142e5403da542b7172b181a
achraf elghinoussi <afaithraf@gmail.com>
2022-01-31 17:12:56 +0100

    fix(hooks): test

--- packages/hooks/src/near-utils.ts
+++ packages/hooks/src/near-utils.ts
@@ -1,9 +1,9 @@
 function getConfig() {
     let config = {
-        networkId: 'testnet',
-        nodeUrl: 'https://rpc.testnet.near.org',
-        walletUrl: 'https://wallet.testnet.near.org',
-        helperUrl: 'https://helper.testnet.near.org',
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        walletUrl: 'https://wallet.mainnet.near.org',
+        helperUrl: 'https://helper.mainnet.near.org',
     }
 
     return config
